f808ebf0ae23b00098ee7aeabf6d997b3ae688b9
root <root@DESKTOP-QS0QAE8>
2023-08-10 16:42:25 +0100

    unique users table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
---creates the user table if it doesn't exist
---or modifies it if it does
+-- creates the user table if it doesn't exist or modifies
+-- it if it does with id, email and name columns
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
