49f71ada4e5e6fc1cc6b9a27eab47cd09680f701
root <root@DESKTOP-QS0QAE8>
2023-08-11 06:32:22 +0100

    err fix

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
 -- Rank country of bands by number of fans
 -- from the metal_band table.
-SELECT country, SUM(fans) as nb_fans 
+SELECT country, COUNT(fans) as nb_fans
+	FROM metal_bands
 	GROUP BY origin 
 	ORDER BY nb_fan DESC;
