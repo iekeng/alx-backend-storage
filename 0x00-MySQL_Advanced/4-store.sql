b45b2640bf538ebedea3e7f592f8af09e7291115
root <root@DESKTOP-QS0QAE8>
2023-08-11 11:37:13 +0100

    err fix

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,6 @@
 -- Create a trigger after an insert on the `order`
 -- table and updates the `items` table
-CREATE TRIGGER red AFTER INSERT ON order
+CREATE TRIGGER red AFTER INSERT ON orders
 	FOR EACH ROW UPDATE items
 	SET quantity = quantity - NEW.number
 	WHERE name = NEW.item_name;
