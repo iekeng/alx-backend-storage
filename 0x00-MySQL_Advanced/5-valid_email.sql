42bc48e9bcaf235bbe2346a5acf7563d1e779776
root <root@DESKTOP-QS0QAE8>
2023-08-11 12:15:26 +0100

    updates valid_email

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- Trigger named res upfated valid_email
+-- if email field is changed
+DELIMITER $$
+
+CREATE TRIGGER res BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email <> OLD.email THEN
+		SET NEW.valid_email = 0
+	END IF;
+END$$
+
+DELIMITER ;
