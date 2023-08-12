35c8e203d8f9ff7332fd4232abd7347daef7453d
root <root@DESKTOP-QS0QAE8>
2023-08-12 20:15:39 +0100

    SafeDiv function

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,9 +2,9 @@
 -- returns their quotient
 DELIMITER $$;
 
-CREATE FUNCTION SafeDiv (a INT, b INT) RETURN FLOAT
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
 BEGIN
-	IF b == 0 THEN
+	IF b = 0 THEN
 		RETURN 0;
 	END IF;
 	RETURN (a/b);
