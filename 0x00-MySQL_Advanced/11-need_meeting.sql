1ac59aff51153562569a0ea4c776a41a067b21d6
root <root@DESKTOP-QS0QAE8>
2023-08-12 23:43:36 +0100

    err fix

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -3,5 +3,4 @@
 CREATE VIEW need_meeting AS
 SELECT name FROM students
 WHERE score < 80 AND
-(last_meeting IS NULL OR
-DATE(CURDATE()) - last_meeting > DATE(INTERVAL 1 MONTH));
+(last_meeting IS NULL OR (DATE(CURDATE()) - last_meeting > DATE(INTERVAL 1 MONTH)));
