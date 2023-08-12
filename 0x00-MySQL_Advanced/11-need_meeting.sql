2299005380c4c7701468e51c90c51560ad433a30
root <root@DESKTOP-QS0QAE8>
2023-08-12 23:37:06 +0100

    need meeting view

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,7 @@
+-- Create need_meeting view that selects
+-- students based on scores and last meeting
+CREATE VIEW need_meeting AS
+SELECT name FROM students
+WHERE score < 80 AND
+(last_meeting IS NULL OR
+DATE(CURDATE()) - last_meeting > DATE(INTERVAL 1 MONTH));
