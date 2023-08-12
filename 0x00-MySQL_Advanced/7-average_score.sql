a82e7c1edd620724e59eef013ac4f16592f55a62
root <root@DESKTOP-QS0QAE8>
2023-08-12 01:40:23 +0100

    err fix

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,11 +1,12 @@
--- fg
--- rr
+-- Creates procedure that computes average score
+-- for students projects
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$;
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	UPDATE users
 	SET average_score=(SELECT AVG(score) FROM corrections
-			   WHERE corrections.user_id = users_id)
+			   WHERE user_id = users_id)
 	WHERE id = user_id;
 	
 
