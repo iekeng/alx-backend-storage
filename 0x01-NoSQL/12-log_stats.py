5fe62c7da311ca9c8061d10265a40e7dc9219ec3
root <root@DESKTOP-QS0QAE8>
2023-08-15 19:45:03 +0100

    query nginx db log

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from pymongo import MongoClient
+'''12-log_stats'''
+
+client = MongoClient()
+db = client['logs']
+nginx = db['nginx']
+
+document_count = nginx.count_documents({})
+get_count = nginx.count_documents({"method": "GET"})
+post_count = nginx.count_documents({"method": "POST"})
+put_count = nginx.count_documents({"method": "PUT"})
+patch_count = nginx.count_documents({"method": "PATCH"})
+delete_count = nginx.count_documents({"method": "DELETE"})
+status_count = nginx.count_documents({"path": "/status"})
+
+print(f"{document_count} logs\nMethods:\n\tmethod GET: {get_count}\
+\n\tmethod POST: {post_count}\n\tmethod PUT: {put_count}\
+\n\tmethod PATCH: {patch_count}\n\tmethod DELETE: {delete_count}\n\
+{status_count} status check")
