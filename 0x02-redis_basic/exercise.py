a255a0ffe41f115cc5f42f2c268329898b6d9221
root <root@DESKTOP-QS0QAE8>
2023-08-17 05:16:32 +0100

    added get methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Redis db"""
 import redis
 from uuid import uuid4 as uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -17,3 +17,23 @@ class Cache:
         key = str(uuid())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float]:
+        '''get method'''
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        elif fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        '''returns a srting when called'''
+        data = self_redis.get(key)
+        return str(data)
+
+    def get_int(self, key: str) -> int:
+        '''returns an int when called'''
+        data = self_redis.get(key)
+        return int(data)
